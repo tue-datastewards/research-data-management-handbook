93cf6bcf3f4df0a7a5cfb79fa3468b8810f468ab
Nami Sunami <nsunami@pm.me>
2025-04-30 13:40:16 +0200

    fix(ds-config): Use PREVIEW_PATH env var to set the base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,9 @@ const config: Config = {
   url: "https://fictional-doodle-vm76q9w.pages.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.GITHUB_ACTIONS ? `${process.env.BASE_URL}/` : "/",
+  baseUrl: process.env.PREVIEW_PATH
+    ? `${process.env.BASE_URL}/${process.env.PREVIEW_PATH}`
+    : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

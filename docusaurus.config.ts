94deeb1c5cd503dd508565080e0043a9bd3a91e9
Nami Sunami <nsunami@pm.me>
2025-04-30 13:06:22 +0200

    chore(docusaurus.config): Update the site URL and the baseUrl According to the actions https://github.com/marketplace/actions/build-deploy-to-github-pages-and-deploy-pr-preview#docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://tue-datastewards.github.io/research-data-management-handbook",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: process.env.GITHUB_ACTIONS ? `${process.env.BASE_URL}/` : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

81be48032967f229eebb388e8d3f8574f3215a34
Nami Sunami <nsunami@pm.me>
2025-05-01 17:15:05 +0200

    feat(deps): Enable docusaurus-lunr-search plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -142,6 +142,7 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  plugins: [require.resolve("docusaurus-lunr-search")],
 }
 
 export default config

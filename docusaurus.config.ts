3acba23fc04e2b2bc97e2366520862a04a2054c5
Nami Sunami <nsunami@pm.me>
2025-05-01 14:28:40 +0200

    chore(ds-config): Update repository URLs in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/tue-datastewards/research-data-management-handbook/tree/main",
         },
         blog: {
           showReadingTime: true,
@@ -55,7 +55,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/tue-datastewards/research-data-management-handbook/tree/main",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
@@ -86,7 +86,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/tue-datastewards/research-data-management-handbook",
           label: "GitHub",
           position: "right",
         },
@@ -130,7 +130,7 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/tue-datastewards/research-data-management-handbook/",
             },
           ],
         },

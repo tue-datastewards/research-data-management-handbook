c3eca6c4467a9767ce2dd3f47ce79d7a197888d4
Nami Sunami <nsunami@pm.me>
2025-05-01 13:28:35 +0200

    Deploy website - based on 8072103d8ea0d084cac0b52ba9b05056d51490d7

--- assets/js/acecf23e.1c2992aa.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
--- pr-preview/pr-17/assets/js/7151302c.ee0b446a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[1069],{1355:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/research-data-management-handbook/pr-preview/pr-17/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);

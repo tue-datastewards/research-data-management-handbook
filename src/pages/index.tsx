939a6a7b367275e8803fa2f6a47cd918f2995f0b
Nami Sunami <nsunami@pm.me>
2025-05-01 12:04:07 +0200

    feat: Add "Learn More" button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            👉 Learn More
           </Link>
         </div>
       </div>

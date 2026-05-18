ebfbbd5077df2692ece2c9a716b22b5e03949532
Nami Sunami <nsunami@pm.me>
2026-05-18 15:28:19 +0200

    feat(component): Add a component for CTA for Research Cockpit

---
+++ src/components/ReadyToStartDMP/index.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+import ScreenshotResearchCockpit from "@site/static/img/screenshot-research-cockpit.jpg";
+import styles from "./styles.module.css";
+
+export default function ReadyToStartDMP(): ReactNode {
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        <img
+          src={ScreenshotResearchCockpit}
+          alt="Screenshot of Research Cockpit"
+          className={styles.screenshot}
+        />
+        <h2>Ready to start your Data Management Plan?</h2>
+        <p>
+          Are you ready to prepare your Data Management Plan? Visit Research
+          Cockpit to start preparing your plan.
+        </p>
+        <p>
+          You can also start an Ethical Approval, or search research tools are
+          available at TU/e.
+        </p>
+        <button
+          className="button button--primary button--lg"
+          onClick={() =>
+            window.open("https://cockpit.research.tue.nl", "_blank")
+          }
+        >
+          Go to Research Cockpit
+        </button>
+      </div>
+    </section>
+  );
+}

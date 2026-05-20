6eb7559273e52edd93e6b7257a84e46b64428c7a
Nami Sunami <nsunami@pm.me>
2026-05-20 19:46:57 +0200

    feat(CreateDMP): Add a button linking out to Cockpit

---
+++ src/components/CreateDMP/index.tsx
@@ -0,0 +1,13 @@
+export default function CreateDMP() {
+  return (
+    <>
+      <a
+        class="button button--primary"
+        href="https://cockpit.research.tue.nl"
+        target="_blank"
+      >
+        Create a Data Management Plan on Research Cockpit
+      </a>
+    </>
+  );
+}
